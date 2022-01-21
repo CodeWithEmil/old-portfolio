a27a4393d49a710e0b8d28b9308b2fb4a2361a87
Emilio <emilionajera23@gmail.com>
2022-01-21 09:26:02 -0600

    Added the parallax-image. It was pretty hard to do, but I think I've finally got it. Will continue to develop the portfolio with the information

--- js/style.js
+++ js/style.js
@@ -18,8 +18,8 @@ let body = document.querySelector("body");
     let icon = document.querySelector("#toggleIcon");
     let bodyDescription = document.querySelector(".description--svg--path");
 
-    let svgDescriptionOne = document.querySelector(".description--content--one--svg--one")
-    let svgDescriptionTwo = document.querySelector(".description--content--one--svg--two")
+    let svgDescriptionOne = document.querySelector(".description--content--one--svg");
+    let svgDescriptionTwo = document.querySelector(".description--content--two--svg");
 
     let root = document.querySelector(':root');
     let clicked = false;
@@ -100,3 +100,75 @@ let body = document.querySelector("body");
 
     console.log(window.innerWidth + " is the inner Width");
     console.log(window.outerWidth + " is the outer Width");
+
+
+
+
+/* 
+    CARD STYLES
+*/
+
+class parallaxTiltEffect {  
+    constructor({element, tiltEffect}) {  
+     this.element = element;  
+     this.container = this.element.querySelector(".wrap--container");  
+     this.size = [300, 360];  
+     [this.w, this.h] = this.size;  
+     this.tiltEffect = tiltEffect;  
+     this.mouseOnComponent = false;  
+     this.handleMouseMove = this.handleMouseMove.bind(this);  
+     this.handleMouseEnter = this.handleMouseEnter.bind(this);  
+     this.handleMouseLeave = this.handleMouseLeave.bind(this);  
+     this.defaultStates = this.defaultStates.bind(this);  
+     this.setProperty = this.setProperty.bind(this);  
+     this.init = this.init.bind(this);  
+     this.init();  
+    }  
+    handleMouseMove(event) {  
+     const {offsetX, offsetY} = event;  
+     let X;  
+     let Y;  
+     if (this.tiltEffect === "reverse") {  
+      X = ((offsetX - (this.w/2)) / 3) / 3;  
+      Y = (-(offsetY - (this.h/2)) / 3) / 3;  
+     }  
+     else if (this.tiltEffect === "normal") {  
+      X = (-(offsetX - (this.w/2)) / 3) / 3;  
+      Y = ((offsetY - (this.h/2)) / 3) / 3;  
+     }  
+     this.setProperty('--rY', X.toFixed(2));  
+     this.setProperty('--rX', Y.toFixed(2));  
+     this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');  
+     this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');  
+    }  
+    handleMouseEnter() {  
+     this.mouseOnComponent = true;  
+     this.container.classList.add("container--active");  
+    }  
+    handleMouseLeave() {  
+     this.mouseOnComponent = false;  
+     this.defaultStates();  
+    }  
+    defaultStates() {  
+     this.container.classList.remove("container--active");  
+     this.setProperty('--rY', 0);  
+     this.setProperty('--rX', 0);  
+     this.setProperty('--bY', '80%');  
+     this.setProperty('--bX', '50%');  
+    }  
+    setProperty(p, v) {  
+     return this.container.style.setProperty(p, v);  
+    }  
+    init() {  
+     this.element.addEventListener('mousemove', this.handleMouseMove);  
+     this.element.addEventListener('mouseenter', this.handleMouseEnter);  
+     this.element.addEventListener('mouseleave', this.handleMouseLeave);  
+    }  
+}  
+const $ = e => document.querySelector(e);  
+const wrap1 = new parallaxTiltEffect({  
+    element: $('.me--wrap'),  
+    tiltEffect: 'reverse'  
+});
+
+console.log(parallaxTiltEffect)
